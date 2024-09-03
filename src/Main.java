3f508146f302ade2cfc976f2602c2a3c2e06bfc0
KulikovMihail <karassuma@yandex.ru>
2024-09-03 14:13:25 +0300

    1,2,3,4,7 Задача

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.print.StreamPrintServiceFactory;
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,16 +7,58 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Задача №1
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int num = in.nextInt();
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите число: ");
+//        int num = in.nextInt();
+//
+//        if (num % 2 == 0){
+//            System.out.println("Число чётное.");
+//        }
+//        else {
+//            System.out.println("Число нечётное.");
+//        }
+        //Задача №2
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите числа для сравнения. Они не должны повторяться. ");
+//        System.out.print("Введите первое число: ");
+//        int first = in.nextInt();
+//        System.out.print("Введите второе число: ");
+//        int second = in.nextInt();
+//        System.out.print("Введите третье число: ");
+//        int third = in.nextInt();
+//
+//        if (first < second & first < third){
+//            System.out.print("Первое число наименьшее.");
+//           }
+//        if (second < first & second < third){
+//            System.out.println("Второе число наименьшее.");
+//        }
+//        if (third < first & third < second){
+//            System.out.println("Третье число наименьшее.");
+//        }
 
-        if (num % 2 == 0){
-            System.out.println("Число чётное.");
-        }
-        else {
-            System.out.println("Число нечётное.");
-        }
-
-        }
+        //Задача 3
+//        int [] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        for (int i = 0; i < numbers.length; i++ ){
+//            System.out.println("5*"+numbers[i]+"="+ 5 * numbers[i]);
+//        }
+        //Задача 4
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите число N : ");
+//        int N = in.nextInt();
+//        int sum = 0;
+//        for (int i = 0; i < N; i++ ){
+//             sum += i;
+//       }
+//        System.out.print("Сумма всех чисел до числа N: "+(sum+N));
+       //Задача 7
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите число N : ");
+//        int N = in.nextInt();
+//        System.out.println("Числа в обратном порядке от числа N: " + N);
+//        int n = N+1;
+//        for (int i = 0; i < N; i++ ) {
+//            System.out.print((n-=1) +" ");
+//        }
     }
+}
